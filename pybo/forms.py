177b73156153c764e8d086a3d89a579a68ad35b6
tngtied <tngtied@gmail.com>
2024-05-26 02:54:53 +0900

    Revert "commit"
    This reverts commit d3f0974ba1879a212f8fadcb32653970c209918b.

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from pybo.models import Question, Answer
+from pybo.models import Question
 
 class QuestionForm(forms.ModelForm):
     class Meta:
@@ -12,12 +12,4 @@ class QuestionForm(forms.ModelForm):
         labels = {
             'subject': '제목',
             'content': '내용',
-        }
-
-class AnswerForm(forms.ModelForm):
-    class Meta:
-        model = Answer 
-        fields = ['content']
-        labels = {
-            'content': '답변내용',
         }
--- pybo/views.py
+++ pybo/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.http import HttpResponseNotAllowed
-from django.core.paginator import Paginator
 from .models import Question
-from .forms import QuestionForm, AnswerForm
+from .forms import QuestionForm
 
 # Create your views here.
 
 def index(request):
-    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    paginator = Paginator(question_list, 10)
-    page_object = paginator.get_page(page)
-
-    context = {'question_list': page_object}
+    context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
@@ -23,18 +17,8 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    if request.method == 'POST':
-        form = AnswerForm(request.POST)
-        if form.is_valid():
-            answer = form.save(commit=False)
-            answer.create_date = timezone.now()
-            answer.question = question
-            answer.save()
-            return redirect('pybo:detail', question_id=question_id)
-    else:
-        return HttpResponseNotAllowed('Method Not Allowed')
-    context = {'question': question, 'form': form}
-    return render(request, 'pybo/question_detail.html', context)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question_id)
 
 def question_create(request):
     if request.method == 'POST':

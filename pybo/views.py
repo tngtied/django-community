bfb39ec35b56ea6f963ce7531a608398664660cc
tngtied <tngtied@gmail.com>
2024-05-26 03:14:49 +0900

    commit

--- pybo/views.py
+++ pybo/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
 from .models import Question
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 
 def index(request):
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)
+    page_object = paginator.get_page(page)
+
+    context = {'question_list': page_object}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
@@ -17,8 +23,18 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        return HttpResponseNotAllowed('Method Not Allowed')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 def question_create(request):
     if request.method == 'POST':

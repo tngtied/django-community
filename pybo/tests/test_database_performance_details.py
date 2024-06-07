e1eb15526185726f7dd00c50a083579a7728909c
tngtied <tngtied@gmail.com>
2024-06-07 18:04:15 +0900

    details 실험 전 세팅

--- pybo/tests/test_database_performance_details.py
+++ pybo/tests/test_database_performance_details.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from pybo.models import Question
+from pybo.models import Question, Answer
 from django.utils import timezone
 from django.contrib.auth.models import User
 from ..decorators import log_time
@@ -18,21 +18,29 @@ class QuestionTest(TestCase):
             password='1234',
             email = 'test@gmail.com'
         )
-        questions = []
+        cls.question = Question(
+            subject='test subject',
+            content='test content',
+            author=cls.author_instance,
+            create_date=timezone.now()
+        )
+        cls.question.save()
+        answers = []
         for i in range(1000000):
-            question = Question(
-                subject='test subject',
+            answer = Answer.objects.create(
+                question=cls.question,
                 content='test content',
                 author=cls.author_instance,
                 create_date=timezone.now()
             )
-            questions.append(question)
-        Question.objects.bulk_create(questions)
+        Answer.objects.bulk_create(answers)
     def test_get_question_page_1(self):
-        print("test_get_question_page_1")
-        response = self.client.get('/?page=1')
+        print("test_get_question_answer_page_1")
+        response = self.client.get('/pybo/1/?page=1')
         self.assertEqual(response.status_code, 200)
     def test_get_question_page_100000(self):
-        print("test_get_question_page_100000")
-        response = self.client.get('/?page=100000')
+        print("test_get_question_answer_page_100000")
+        response = self.client.get('/pybo/1/?page=100000')
+        # print(response.body)
+        # print(response.content)
         self.assertEqual(response.status_code, 200)
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -12,7 +12,7 @@ def index(request):
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
     question_list = Question.objects.order_by('-create_date')
-    print(question_list.query)
+    print(f">> 쿼리: {question_list.query}")
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |
@@ -21,10 +21,10 @@ def index(request):
             Q(author__username__icontains=kw) |
             Q(answer__author__username__icontains=kw)
         ).distinct()
+    print(f"type(int(page)) {type(int(page))}, value {int(page)}")
 
     paginator = Paginator(question_list, 10)
     page_object = paginator.get_page(page)
-
     context = {'question_list': page_object, 'page': page, 'kw' : kw}
     return {'context': context, 'template': 'pybo/question_list.html'}
 
@@ -36,8 +36,8 @@ def detail(request, question_id):
     question.hits += 1
     question.save()
     answer_list = question.answer_set.all().annotate(num_voter = Count('voter')).order_by('-num_voter', '-create_date')
-
-    paginator = Paginator(answer_list, 5)
+    print(f"query of answer_list: {answer_list.query}")
+    paginator = Paginator(answer_list, 10)
     page_object = paginator.get_page(page)
     # print("page object %s" % page_object.count)
 
